7ba70c90e36e0b94a049069a4d6cad72d1853e8c
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-19 19:32:02 +0530

    import

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./chat.css";
 import EmojiPicker from "emoji-picker-react";
+import {
+  arrayUnion,
+  doc,
+  getDoc,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../../lib/firebase";
+import { useChatStore } from "../../lib/chatStore";
+import { useUserStore } from "../../lib/userStore";
+import upload from "../../lib/upload";
+import { format } from "timeago.js";
+
 
 const Chat = () => {
   const [open, setOpen] = useState(false);
