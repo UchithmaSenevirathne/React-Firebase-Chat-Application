88bfc804ab58ad1564aaa6b977c90b6d5c483e08
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-19 19:06:09 +0530

    sign in

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -29,6 +29,13 @@ const Login = () => {
 
     const formData = new FormData(e.target);
     const { email, password } = Object.fromEntries(formData);
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.log(err);
+      toast.error(err.message);
+    }
   };
 
   const handleRegister = async (e) => {
