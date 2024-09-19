1b1ef76fb1bc97664a341ab237b98861e8a47df2
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-19 19:10:56 +0530

    import userstore

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -14,6 +14,7 @@ import {
   where,
 } from "firebase/firestore";
 import { useState } from "react";
+import { useUserStore } from "../../../../lib/userStore";
 
 const AddUser = () => {
   return (
