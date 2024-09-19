64690df13af5e35b449e853c251ddb98d7d92e90
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-19 19:45:46 +0530

    update some content

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,18 @@ const App = () => {
   const { currentUser, isLoading, fetchUserInfo } = useUserStore();
   const { chatId } = useChatStore();
 
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      fetchUserInfo(user?.uid);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+
   return (
     <div className="container">
       {user ? (
