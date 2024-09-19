80858eaf8d29a9315c8d14bab66b376db068e536
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-19 09:52:03 +0530

    craete event for change addMode

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import "./chatList.css"
 
 const ChatList = () => {
+  const [addMode, setAddMode] = useState(false)
   return (
     <div className='chatList'>
       <div className="search">
@@ -9,7 +10,8 @@ const ChatList = () => {
           <img src="./search.png" alt="" />
           <input type="text" placeholder='Search'/>
         </div>
-        <img src="./plus.png" alt="" className='add'/>
+        <img src={addMode ? "./minus.png" : "./plus.png" }alt="" className='add'
+        onClick={() => setAddMode((prev) => !prev)}/>
       </div>
     </div>
   )
