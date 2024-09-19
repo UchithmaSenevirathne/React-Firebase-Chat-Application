5b92997ddf8fbfeb839c04a8e46bf04cb5008584
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-19 19:42:42 +0530

    update details

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -32,72 +32,96 @@ const Detail = () => {
 
   return (
     <div className="detail">
-      <div className="user">
-        <img src="./avatar.png" alt="" />
-        <h2>Jane Doe</h2>
-        <p>Lorem ipsum dolor sit amet.</p>
-      </div>
-      <div className="info">
-        <div className="option">
-          <div className="title">
-            <span>Chat Settings</span>
-            <img src="./arrowUp.png" alt="" />
-          </div>
-        </div>
-        <div className="option">
-          <div className="title">
-            <span>Privacy & help</span>
-            <img src="./arrowUp.png" alt="" />
-          </div>
-        </div>
-        <div className="option">
-          <div className="title">
-            <span>Shared photos</span>
-            <img src="./arrowDown.png" alt="" />
-          </div>
-          <div className="photos">
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img
-                  src="https://images.pexels.com/photos/27745133/pexels-photo-27745133/free-photo-of-green-big-leaves-of-plant.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                  alt=""
-                />
-                <span>photo_2024_2.png</span>
-              </div>
-              <img src="./download.png" alt="" className="icon"/>
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img
-                  src="https://images.pexels.com/photos/27745133/pexels-photo-27745133/free-photo-of-green-big-leaves-of-plant.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                  alt=""
-                />
-                <span>photo_2024_2.png</span>
-              </div>
-              <img src="./download.png" alt="" className="icon"/>
-            </div>
-            <div className="photoItem">
-              <div className="photoDetail">
-                <img
-                  src="https://images.pexels.com/photos/27745133/pexels-photo-27745133/free-photo-of-green-big-leaves-of-plant.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                  alt=""
-                />
-                <span>photo_2024_2.png</span>
-              </div>
-              <img src="./download.png" alt="" className="icon"/>
-            </div>
-          </div>
-        </div>
-        <div className="option">
-          <div className="title">
-            <span>Shared files</span>
-            <img src="./arrowUp.png" alt="" />
-          </div>
-        </div>
-        <button>Block User</button>
-        <button className="logout">Logout</button>
-      </div>
+    <div className="user">
+      <img src={user?.avatar || "./avatar.png"} alt="" />
+      <h2>{user?.username}</h2>
+      <p>Lorem ipsum dolor sit amet.</p>
     </div>
+    <div className="info">
+      <div className="option">
+        <div className="title">
+          <span>Chat Settings</span>
+          <img src="./arrowUp.png" alt="" />
+        </div>
+      </div>
+      <div className="option">
+        <div className="title">
+          <span>Chat Settings</span>
+          <img src="./arrowUp.png" alt="" />
+        </div>
+      </div>
+      <div className="option">
+        <div className="title">
+          <span>Privacy & help</span>
+          <img src="./arrowUp.png" alt="" />
+        </div>
+      </div>
+      <div className="option">
+        <div className="title">
+          <span>Shared photos</span>
+          <img src="./arrowDown.png" alt="" />
+        </div>
+        <div className="photos">
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img
+                src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                alt=""
+              />
+              <span>photo_2024_2.png</span>
+            </div>
+            <img src="./download.png" alt="" className="icon" />
+          </div>
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img
+                src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                alt=""
+              />
+              <span>photo_2024_2.png</span>
+            </div>
+            <img src="./download.png" alt="" className="icon" />
+          </div>
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img
+                src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                alt=""
+              />
+              <span>photo_2024_2.png</span>
+            </div>
+            <img src="./download.png" alt="" className="icon" />
+          </div>
+          <div className="photoItem">
+            <div className="photoDetail">
+              <img
+                src="https://images.pexels.com/photos/7381200/pexels-photo-7381200.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                alt=""
+              />
+              <span>photo_2024_2.png</span>
+            </div>
+            <img src="./download.png" alt="" className="icon" />
+          </div>
+        </div>
+      </div>
+      <div className="option">
+        <div className="title">
+          <span>Shared Files</span>
+          <img src="./arrowUp.png" alt="" />
+        </div>
+      </div>
+      <button onClick={handleBlock}>
+        {isCurrentUserBlocked
+          ? "You are Blocked!"
+          : isReceiverBlocked
+          ? "User blocked"
+          : "Block User"}
+      </button>
+      <button className="logout" onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  </div>
   );
 };
 
