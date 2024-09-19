bdaa83a710e9630d7fb8b46d6036a5d99f3a689e
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-19 19:24:58 +0530

    update chat list

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -68,52 +68,53 @@ const ChatList = () => {
   );
 
   return (
-    <div className='chatList'>
-      <div className="search">
-        <div className="searchBar">
-          <img src="./search.png" alt="" />
-          <input type="text" placeholder='Search'/>
-        </div>
-        <img src={addMode ? "./minus.png" : "./plus.png" }alt="" className='add'
-        onClick={() => setAddMode((prev) => !prev)}/>
+    <div className="chatList">
+    <div className="search">
+      <div className="searchBar">
+        <img src="./search.png" alt="" />
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setInput(e.target.value)}
+        />
       </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Jane Doe</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Jane Doe</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Jane Doe</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Jane Doe</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="item">
-        <img src="./avatar.png" alt="" />
-        <div className="texts">
-          <span>Jane Doe</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      {addMode && <AddUser/>}
+      <img
+        src={addMode ? "./minus.png" : "./plus.png"}
+        alt=""
+        className="add"
+        onClick={() => setAddMode((prev) => !prev)}
+      />
     </div>
+    {filteredChats.map((chat) => (
+      <div
+        className="item"
+        key={chat.chatId}
+        onClick={() => handleSelect(chat)}
+        style={{
+          backgroundColor: chat?.isSeen ? "transparent" : "#5183fe",
+        }}
+      >
+        <img
+          src={
+            chat.user.blocked.includes(currentUser.id)
+              ? "./avatar.png"
+              : chat.user.avatar || "./avatar.png"
+          }
+          alt=""
+        />
+        <div className="texts">
+          <span>
+            {chat.user.blocked.includes(currentUser.id)
+              ? "User"
+              : chat.user.username}
+          </span>
+          <p>{chat.lastMessage}</p>
+        </div>
+      </div>
+    ))}
+
+    {addMode && <AddUser />}
+  </div>
   )
 }
 
