92e5fb614119a21952ea0522725a9e6a45efcf3c
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-19 07:48:17 +0530

    add user icons to user info section

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./list.css"
+import UserInfo from "./userInfo/UserInfo"
+import ChatList from "./chatList/ChatList"
 
 const List = () => {
   return (
-    <div className='list'>List</div>
+    <div className='list'>
+      <UserInfo/>
+      <ChatList/>
+    </div>
   )
 }
 
