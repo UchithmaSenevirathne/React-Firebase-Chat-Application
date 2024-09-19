6db3d63afee2fd6f74b7a4d26c771e233c253721
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-19 10:28:59 +0530

    add css to chat items

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -41,6 +41,13 @@ const ChatList = () => {
           <p>Hello</p>
         </div>
       </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+          <span>Jane Doe</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
   )
 }
