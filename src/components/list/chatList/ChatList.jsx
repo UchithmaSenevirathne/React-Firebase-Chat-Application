e7a4f6a4c3314501119c02340fa3ee8cd26a0345
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-19 09:47:08 +0530

    implement chat list

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./chatList.css"
 
 const ChatList = () => {
   return (
-    <div className='chatList'>ChatList</div>
+    <div className='chatList'>
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder='Search'/>
+        </div>
+        <img src="./plus.png" alt="" className='add'/>
+      </div>
+    </div>
   )
 }
 
